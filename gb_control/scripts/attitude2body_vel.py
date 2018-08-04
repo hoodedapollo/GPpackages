f777a480f4ad61be08594e765efe98c75bbb8f8c
hoodedapollo <marco.ruzzon@mail.polimi.it>
2018-08-04 17:05:32 +0200

    fix indentation problems due to editor

--- gb_control/scripts/attitude2body_vel.py
+++ gb_control/scripts/attitude2body_vel.py
@@ -33,7 +33,7 @@ class GestureBasedBehaviour():
         #Last acelleration data received
 
 		self.last_acc = [0,0,0]
-        	self.rpy = [0,0,0]
+		self.rpy = [0,0,0]
 
 	#Parameter to set in the launch file to switch from the direct control ( linear acceleration ) to the attitude control ( roll pitch yaw )
 		self.switch=rospy.get_param('~gb_control_type')
@@ -42,70 +42,67 @@ class GestureBasedBehaviour():
 		self.body_vel=Twist()
 
 
-        	self.subAttitude = rospy.Subscriber('/rpy_deg',Vector3Stamped,self.callbackAttitude,queue_size=1)
+		self.subAttitude = rospy.Subscriber('/rpy_deg',Vector3Stamped,self.callbackAttitude,queue_size=1)
 		self.subLinear = rospy.Subscriber('/imu/data_raw',Imu,self.callbackLinear,queue_size=1)
-        	self.pub_gesture_control = rospy.Publisher('/gesture_based_behaviour', Twist, queue_size=0)
+		self.pub_gesture_control = rospy.Publisher('/gesture_based_behaviour', Twist, queue_size=0)
 		
 
-	
+#Mapping from linear acceleration to MiRo body velocity	
 	def callbackLinear(self,sm_data):
 
 		self.last_acc[0] = sm_data.linear_acceleration.x
-
-	        self.last_acc[1] = sm_data.linear_acceleration.y
-        	self.last_acc[2] = sm_data.linear_acceleration.z
+		self.last_acc[1] = sm_data.linear_acceleration.y
+		self.last_acc[2] = sm_data.linear_acceleration.z
 		
 		
         
-        	if  -2 < self.last_acc[0] < 2 and -2 < self.last_acc[1] < 2:
-            		self.body_vel.linear.x=0.0
-            		self.body_vel.angular.z=0.0
-            		print ('MiRo STAY Still')
-
-	    	if -8 < self.last_acc[0] > 8:
+		if  -2 < self.last_acc[0] < 2 and -2 < self.last_acc[1] < 2:
+			self.body_vel.linear.x=0.0
+			self.body_vel.angular.z=0.0
+			print ('MiRo STAY Still')
+		
+		if -8 < self.last_acc[0] > 8:
 
 			self.body_vel.linear.x=-self.last_acc[0]*50
 			self.body_vel.angular.z=0.0
+		
+		else:
 
-	    	else:
-        		self.body_vel.linear.x=-self.last_acc[0]*50
-        		self.body_vel.angular.z=self.last_acc[1]*0.5
-        		print ('MiRo Go normal')
+			self.body_vel.linear.x=-self.last_acc[0]*50
+			self.body_vel.angular.z=self.last_acc[1]*0.5
+			print ('MiRo Go normal')
 		
 			
  
 	def callbackAttitude(self,sw_data):
 
-        	self.rpy[0] = sw_data.vector.x
-        	self.rpy[1] = sw_data.vector.y
-        	self.rpy[2] = sw_data.vector.z
+		self.rpy[0] = sw_data.vector.x
+		self.rpy[1] = sw_data.vector.y
+		self.rpy[2] = sw_data.vector.z
 
 
 		newroll=self.rpy[0]+70
 
-        	if  -15 < newroll < 15 and -20 < self.rpy[1] < 20:
-               		self.sw_vel.linear.x=0.0
-             		self.sw_vel.angular.z=0.0
-            		print ('MiRo STAY Still')
+		if  -15 < newroll < 15 and -20 < self.rpy[1] < 20:
+			self.sw_vel.linear.x=0.0
+			self.sw_vel.angular.z=0.0
+			print ('MiRo STAY Still')
 
-        	elif self.rpy[1] > 40 or self.rpy[1] < -60:
+		elif self.rpy[1] > 40 or self.rpy[1] < -60:
+			self.sw_vel.linear.x=self.rpy[1]*6
+			self.sw_vel.angular.z=0.0
+			print ( 'max linear velocity')
 
-            		self.sw_vel.linear.x=self.rpy[1]*6
-            		self.sw_vel.angular.z=0.0
-            		print ( 'max linear velocity')
-
- 
-        	else:
-
-            		self.sw_vel.linear.x=0.0
-            		self.sw_vel.angular.z=-newroll*0.8*pi/180
-            		print ('MiRo follow your owner')
+		else:
+			self.sw_vel.linear.x=0.0
+			self.sw_vel.angular.z=-newroll*0.8*pi/180
+			print ('MiRo follow your owner')
 
 
 	#Switching between the two possible control based on the variable on the launch file
 	def loop(self):
-
-        	while not rospy.is_shutdown():
+	
+		while not rospy.is_shutdown():
 
 			if self.switch == 'linear':
 				self.pub_gesture_control.publish(self.body_vel)
